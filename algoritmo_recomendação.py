0b318a8c52bc51cce659bec97d38184cf4fec279
Vitor Flores <85088777+Vitorfl0res25@users.noreply.github.com>
2023-11-22 00:18:40 -0300

    algoritmo_recomendação.py

--- algoritmo_recomendação.py
+++ algoritmo_recomendação.py
@@ -3,8 +3,7 @@ from firebase_admin import credentials, firestore, initialize_app
 from datetime import datetime
 import spacy
 import random
-
-
+from werkzeug.utils import url_quote
 
 app = Flask(__name__)
 
